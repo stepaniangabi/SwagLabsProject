a2b2914d773f611d4fbe83343eada0c985c6e414
stepaniangabi <stepaniangabriellarobertha@gmail.com>
2026-03-19 23:23:23 +0200

    updates

--- src/test/java/sharedData/SharedData.java
+++ src/test/java/sharedData/SharedData.java
@@ -2,18 +2,33 @@ package sharedData;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class SharedData {
     private WebDriver driver;
 
     @BeforeMethod
     public void prepareEnvironment(){
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        Map<String, Object> prefs = new HashMap<>();
+        prefs.put("credentials_enable_service", false);                 // disable Save passwords
+        prefs.put("profile.password_manager_enabled", false);           // disable password manager
+        prefs.put("profile.password_manager_leak_detection", false);    // disable breach warning
+
+        options.setExperimentalOption("prefs", prefs);
+
+        driver = new ChromeDriver(options);
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
+
     }
+
 
     @AfterMethod
     public void clearEnvironment(){
